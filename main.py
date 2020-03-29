9ba929312017f795ee9c5a13a4c7fccb7f3ebdc5
Ryan Han <ryan.han@uwaterloo.ca>
2020-03-28 20:41:29 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from crawler import crawl
+from crawler import getlinks
+
+keywords = ['Trump']
+num_of_news = 27338
+links_path = 'links.txt'
+news_path = 'news.txt'
+
+if __name__ == "__main__":
+    
+    finished = []
+    for keyword in keywords:
+
+        getlinks.getlinks(keyword, num_of_news, links_path)
+        crawl.crawl(links_path, news_path)
+        
+        finished.append(keyword)
+        print('---------------\n')
+        print('Finished crawling keyword(s): {}'.format(str(finished)))
